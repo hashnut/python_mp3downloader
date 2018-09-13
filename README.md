a989450be2bfc2b0ffec6d4fb556f7a6390369ce
hashnut <ejrrb8@gmail.com>
2018-09-13 11:13:49 +0900

    geckodriver

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # python_mp3downloader
+
+# 파이썬 webdriver module, Firefox를 이용하여 커맨드 라인에서 간편하게 유투브 음악을 mp3 형태로 다운받습니다.
+
+### Prerequsite :
+
+- Python3 (Latest version is better)
+
+- geckodriver.exe (alreadty contained, but you need to **add Path in System variable** where your it lives)
+
+- 3rd party modules (pyperclip, etc)
+
+- Add path of mp3.py in System variables
+
+### How it works?
+
+원하는 검색어나 유투브 링크를 복사한 상태에서 cmd 창에 'mp3'를 누르고 엔터를 치면 됩니다.
+
+또는, 'mp3 some love song' 처럼 원하는 키워드를 바로 입력해도 됩니다.
+
+파이어폭스 브라우저가 열리며 자동으로 다운로드가 시작하고, 후에 파이어폭스가 자동 종료 됩니다.
--- test.py
+++ test.py
@@ -1,4 +1,33 @@
 #! python3
 # test.py - Launches a map in the browser using an address from the
-# command line or clipboard.
+# command line or clipboard. (https://y2mate.com)
+# mp3 too good at goodbyes
+# mp3
+# my love / lose yourself / havana trump cover
 
+import webbrowser, sys, requests
+import pyperclip
+
+if len(sys.argv) > 1:
+     # Get user input arguments and combine as single string
+     keyword = '%20'.join(sys.argv[1:])
+     print(keyword)
+else:
+     # Get song info or url from clipboard
+     keyword = pyperclip.paste()
+
+search_link = 'https://y2mate.com/search/' + keyword
+
+
+
+# check internet connection
+res = requests.get(serach_link)
+res.raise_for_status()
+
+mp3Soup = bs4.BeautifulSoup(res.text)
+elem_downLink = mp3Soup.select('.item-thumbnail')
+
+
+
+# diplay text while downloading mp3 file
+print('Downloading...')  
